b234fd590d75d0e2f93989b2b4fbf70022ae5ea1
Jonathan Sick <jsick@lsst.org>
2019-10-25 13:57:48 -0400

    create_k8sclient falls back to kube config
    This should be better for testing and development since it will
    automatically fall back to use a kubectl configuration if the pods
    security account isn't available.

--- strimziregistryoperator/k8stools.py
+++ strimziregistryoperator/k8stools.py
@@ -3,10 +3,10 @@ __all__ = ('create_k8sclient',)
 import kubernetes
 
 
-def create_k8sclient(incluster=True):
-    if incluster:
+def create_k8sclient():
+    try:
         kubernetes.config.load_incluster_config()
-    else:
+    except Exception:
         kubernetes.config.load_kube_config()
     kubernetes.client.configuration.assert_hostname = False
     return kubernetes.client
