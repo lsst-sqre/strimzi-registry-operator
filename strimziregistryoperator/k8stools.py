dcdbc1567b831914ff4cc750e892d3b4b3000973
Jonathan Sick <jsick@lsst.org>
2019-10-29 16:56:42 -0400

    Add generic helpers for getting resources

--- strimziregistryoperator/k8stools.py
+++ strimziregistryoperator/k8stools.py
@@ -1,4 +1,6 @@
-__all__ = ('create_k8sclient',)
+__all__ = ('create_k8sclient', 'get_deployment', 'get_service')
+
+import json
 
 import kubernetes
 
@@ -10,3 +12,37 @@ def create_k8sclient():
         kubernetes.config.load_kube_config()
     kubernetes.client.configuration.assert_hostname = False
     return kubernetes.client
+
+
+def get_deployment(*, name, namespace, k8s_client, raw=True):
+    if raw:
+        preload_content = False
+    else:
+        preload_content = True
+
+    api = k8s_client.AppsV1Api()
+    result = api.read_namespaced_deployment(
+        name=name,
+        namespace=namespace,
+        _preload_content=preload_content)
+    if raw:
+        return json.loads(result.data)
+    else:
+        return result
+
+
+def get_service(*, name, namespace, k8s_client, raw=True):
+    if raw:
+        preload_content = False
+    else:
+        preload_content = True
+
+    api = k8s_client.CoreV1Api()
+    result = api.read_namespaced_service(
+        name=name,
+        namespace=namespace,
+        _preload_content=preload_content)
+    if raw:
+        return json.loads(result.data)
+    else:
+        return result
